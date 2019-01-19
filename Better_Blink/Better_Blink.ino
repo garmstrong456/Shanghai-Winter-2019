bd0735957eac98f0bfd11cd30a4376fee44ed083
Greg <garmstrong456@gmail.com>
2019-01-18 22:08:38 -0500

    Better Blink

---
+++ Better_Blink/Better_Blink.ino
@@ -0,0 +1,50 @@
+#include <M5Stack.h>
+
+#define LED_PIN 2
+#define LED_BLINK_TIME 500
+#define SCREEN_BLINK_TIME 200
+
+bool ledOnOrOff = LOW;
+bool ledState = LOW;
+bool screenState = LOW;
+unsigned long currentTime;
+unsigned long nextLEDBlinkTime;
+unsigned long nextScreenBlinkTime;
+
+void setup() {
+  M5.begin();
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  M5.update();
+  
+  /*READ INPUTS*/
+  currentTime = millis();   //number of milliseconds since the program started running
+  if (M5.wasPressed()) {
+    ledOnOrOff = !ledOnOrOff;
+  }
+
+  /*UPDATE STATE*/
+  if (currentTime >= nextLEDBlinkTime) {
+    nextLEDBlinkTime = currentTime + LED_BLINK_TIME;
+    ledState = !ledState;
+  }
+  if (currentTime >= nextScreenBlinkTime) {
+    nextScreenBlinkTime = currentTime + SCREEN_BLINK_TIME;
+    screenState = !screenState;
+  }
+
+  /*SET OUTPUTS*/
+  if (ledOnOrOff) {
+    digitalWrite(LED_PIN, ledState);
+  } else {
+    digitalWrite(LED_PIN, LOW);
+  }
+
+  if (screenState) {
+    M5.Lcd.fillScreen(RED);
+  } else {
+    M5.Lcd.fillScreen(BLUE);
+  }
+}
